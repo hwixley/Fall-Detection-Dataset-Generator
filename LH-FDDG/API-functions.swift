1dbc7257da73b5c2f54928b44f04a02516ca217c
Harry Wixley <hwixley1@gmail.com>
2022-01-18 18:25:13 +0000

    localhost server data get working

--- LH-FDDG/API-functions.swift
+++ LH-FDDG/API-functions.swift
@@ -12,33 +12,84 @@ let ip = "192.168.8.171"
 let port = "8081"
 let host = "http://" + ip + ":" + port
 
-struct Recording {
+struct Coord: Codable {
+    var x: [Double] // x, roll
+    var y: [Double]  // y, pitch
+    var z: [Double]  // z, yaw
+}
+
+struct Recording: Codable {
+    var creation: String
     var _id: String
-    var subject_id: String
-    var fall_time: Float
-    var fall_type: String
-    var recording_duration: Float
-    var ground_time: Float
-    var p_ecg: [Float]
-    var p_acc_x: [Float]
-    var p_acc_y: [Float]
-    var p_acc_z: [Float]
-    var acc_x: [Float]
-    var acc_y: [Float]
-    var acc_z: [Float]
-    var gyr_x: [Float]
-    var gyr_y: [Float]
-    var gyr_z: [Float]
-    var gra_x: [Float]
-    var gra_y: [Float]
-    var gra_z: [Float]
-    var mag_x: [Float]
-    var mag_y: [Float]
-    var mag_z: [Float]
-    var att_roll: [Float]
-    var att_pitch: [Float]
-    var att_yaw: [Float]
-    var delta_heading: [Float]
+    //var subject_id: String
+    //var fall_time: Double
+    //var fall_type: String
+    //var recording_duration: Double
+    //var ground_time: Double
+    var p_ecg: [Double]
+    var p_acc_x: [Double]
+    var p_acc_y: [Double]
+    var p_acc_z: [Double]
+    var acc_x: [Double]
+    var acc_y: [Double]
+    var acc_z: [Double]
+    var gyr_x: [Double]
+    var gyr_y: [Double]
+    var gyr_z: [Double]
+    var gra_x: [Double]
+    var gra_y: [Double]
+    var gra_z: [Double]
+    var mag_x: [Double]
+    var mag_y: [Double]
+    var mag_z: [Double]
+    var att_roll: [Double]
+    var att_pitch: [Double]
+    var att_yaw: [Double]
+    var delta_heading: [Double]
+    /*
+    private enum CodingKeys: String, CodingKey {
+        case creation
+        case _id
+        case fall_time
+        case fall_type
+        case recording_duration
+        case ground_time
+        case p_ecg
+        case p_acc_x
+        case p_acc_y
+        case p_acc_z
+        case acc_x
+        case acc_y
+        case acc_z
+        case gyr_x
+        case gyr_y
+        case gyr_z
+        case gra_x
+        case gra_y
+        case gra_z
+        case mag_x
+        case mag_y
+        case mag_z
+        case att_roll
+        case att_pitch
+        case att_yaw
+        case delta_heading
+    }
+    
+    init(creation: NSDate, _id: String, fall_time: String) {
+        <#statements#>
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        creation = try values.decode(NSDate, forKey: .creation)
+        _id = try values.decode(String, forKey: ._id)
+        fall_time = try values.decode(Float, forKey: .fall_time)
+        fall_type = try values.decode(String, forKey: .fall_type)
+        recording_duration = try values.decode(Float, forKey: .recording_duration)
+        ground_time = try values.decode(Float, forKey: .ground_time)
+        p_ecg = try values.decode([Float], forKey: .p_ecg)
+    }*/
 }
 
 struct User {
@@ -53,15 +104,21 @@ struct User {
 }
 
 class APIFunctions {
+    
+    var delegate: DataDelegate?
+    static let functions = APIFunctions()
+    
     func fetchRecordings() {
         print("fetching user data...")
         AF.request(host + "/fetchRecordings").response { response in
             print(response.data ?? "null data")
             
+            let data = String(data: response.data!, encoding: .utf8)
+            print(data)
             
-            //print(JSONDecoder.decode(<#T##self: JSONDecoder##JSONDecoder#>))
+            self.delegate?.fetchRecordings(recordings: data!)
             
-            //let data = String(data: response.data!, encoding: .utf8)
+            //print(JSONDecoder().decode([Recording].self, from: data!.data(using: .utf8)!))
         }
     }
 }
--- LH-FDDG/ViewControllers/StartViewController.swift
+++ LH-FDDG/ViewControllers/StartViewController.swift
@@ -7,15 +7,22 @@
 
 import UIKit
 
+protocol DataDelegate {
+    func fetchRecordings(recordings: String)
+}
+
 class StartViewController: UIViewController {
+    
+    var recordingsArray = [Recording]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         getConstantData()
         
-        let api = APIFunctions()
-        api.fetchRecordings()
+        APIFunctions.functions.delegate = self
+        APIFunctions.functions.fetchRecordings()
+        print(recordingsArray)
     }
     
 
@@ -23,4 +30,17 @@ class StartViewController: UIViewController {
     @IBAction func unwindToStart(segue: UIStoryboardSegue) {
     }
 
+}
+
+
+extension StartViewController: DataDelegate {
+    
+    func fetchRecordings(recordings: String) {
+        do {
+            recordingsArray = try JSONDecoder().decode([Recording].self, from: recordings.data(using: .utf8)!)
+            print(recordingsArray)
+        } catch {
+            print("Failed to decode")
+        }
+    }
 }
